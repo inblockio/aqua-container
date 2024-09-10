660a1e98197869c987f1ae6c7290bd4373182cbc
lat-murmeldjur <lat.murmeldjur@gmail.com>
2024-09-10 12:24:43 +0200

    feat: verification hashes

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,7 @@ use bonsaidb::local::Database;
 use serde::{Deserialize, Serialize};
 
 use guardian_common::{crypt, custom_types::*};
+use verifier::v1_1::hashes::*;
 
 const UPLOADS_DIRECTORY: &str = "uploads";
 
@@ -80,6 +81,8 @@ async fn save_request_body(
     tracing::debug!("yay2");
 
     while let Some(field) = multipart.next_field().await.unwrap() {
+        println!("{:#?}", field);
+
         let name = field.name().unwrap().to_string();
         let file_name = field.file_name().unwrap().to_string();
         let content_type = field.content_type().unwrap().to_string();
@@ -88,53 +91,58 @@ async fn save_request_body(
         let mut content_hasher = sha3::Sha3_512::default();
         // 4.b add rev.metadata.domain_id to hasher {m}
         content_hasher.update(body_bytes.clone());
-        let content_hash = Hash::from(content_hasher.finalize());
+        let content_hash_current = Hash::from(content_hasher.finalize());
 
         let b64 = Base64::from(body_bytes);
+        let domain_id_current = "0".to_owned();
+        let timestamp_current = Timestamp::from(chrono::NaiveDateTime::from_timestamp(
+            Utc::now().timestamp(),
+            0,
+        ));
+
+        let metadata_hash_current =
+            metadata_hash(&domain_id_current, &timestamp_current.clone(), None);
+
+        let verification_hash_current =
+            verification_hash(&content_hash_current, &metadata_hash_current, None, None);
+
+        let pagedata_current = &PageData {
+            pages: vec![HashChain {
+                genesis_hash: verification_hash_current.clone().to_string(),
+                domain_id: domain_id_current,
+                title: file_name.clone(),
+                namespace: 0,
+                chain_height: 0,
+                revisions: vec![(
+                    verification_hash_current,
+                    Revision {
+                        content: RevisionContent {
+                            file: Some(FileContent {
+                                data: b64,
+                                filename: file_name.clone(),
+                                size: 0,
+                                comment: "".to_string(),
+                            }),
+                            content: BTreeMap::new(),
+                            content_hash: content_hash_current,
+                        },
+                        metadata: RevisionMetadata {
+                            domain_id: "0".to_string(),
+                            time_stamp: timestamp_current,
+                            previous_verification_hash: None,
+                            metadata_hash: metadata_hash_current,
+                            verification_hash: verification_hash_current,
+                        },
+                        signature: None,
+                        witness: None,
+                    },
+                )],
+            }],
+        };
 
         let document = &server_database
             .db
-            .set_key(
-                file_name.clone(),
-                &PageData {
-                    pages: vec![HashChain {
-                        genesis_hash: "".to_owned(),
-                        domain_id: "".to_owned(),
-                        title: "".to_owned(),
-                        namespace: 0,
-                        chain_height: 0,
-                        revisions: vec![(
-                            content_hash,
-                            Revision {
-                                content: RevisionContent {
-                                    file: Some(FileContent {
-                                        data: b64,
-                                        filename: "Test".to_string(),
-                                        size: 0,
-                                        comment: "".to_string(),
-                                    }),
-                                    content: BTreeMap::new(),
-                                    content_hash: content_hash,
-                                },
-                                metadata: RevisionMetadata {
-                                    domain_id: "0".to_string(),
-                                    time_stamp: Timestamp::from(
-                                        chrono::NaiveDateTime::from_timestamp(
-                                            Utc::now().timestamp(),
-                                            0,
-                                        ),
-                                    ),
-                                    previous_verification_hash: None,
-                                    metadata_hash: content_hash,
-                                    verification_hash: content_hash,
-                                },
-                                signature: None,
-                                witness: None,
-                            },
-                        )],
-                    }],
-                },
-            )
+            .set_key(file_name.clone(), &pagedata_current)
             .only_if_vacant()
             .execute()
             .unwrap();
@@ -148,10 +156,13 @@ async fn save_request_body(
             if rev1.content.file.is_some() {
                 let file: FileContent = rev1.content.file.clone().unwrap();
                 let vu8 = file.data.to_vec();
-                println!("{:#?}", String::from_utf8(vu8));
+                // println!("{:#?}", String::from_utf8(vu8));
             }
+
+            print!("{:#?}", serde_json::to_value(&doc).unwrap());
         }
     }
+
     Ok(Redirect::to("/"))
 }
 
