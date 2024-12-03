4880c3cad48c1f050fec1de647581576d4846d3c
arthur-kamau <kamaukenn11@gmail.com>
2024-12-03 10:59:54 +0300

    1. edit readme with helpfull command and remove un needed commands 2. added a dockerignore file

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -9,8 +9,8 @@ export default defineConfig({
     nodePolyfills()
   ],
    // Optional: add fallback values
-   define: {
-    'import.meta.env.VITE_REMOTE': JSON.stringify(process.env.VITE_REMOTE || '127.0.0.1'),
-    'import.meta.env.VITE_REMOTE_PORT': JSON.stringify(process.env.VITE_REMOTE_PORT || '3600')
-  }
+  //  define: {
+  //   'import.meta.env.VITE_REMOTE': JSON.stringify(process.env.VITE_REMOTE || '127.0.0.1'),
+  //   'import.meta.env.VITE_REMOTE_PORT': JSON.stringify(process.env.VITE_REMOTE_PORT || '3600')
+  // }
 })
