d46598fc0acf7624838700ecabb38c7091d265cf
arthur-kamau <kamaukenn11@gmail.com>
2025-01-23 16:57:03 +0300

    fix db crud operations

--- src/db/settings.rs
+++ src/db/settings.rs
@@ -2,33 +2,34 @@ use crate::models::database_models::SettingsTable;
 use crate::schema::Settings;
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
+use diesel::r2d2::{ConnectionManager, PooledConnection};
+use crate::schema::Settings::dsl::*;
 
-pub fn create_setting(conn: &PgConnection, new_setting: SettingsTable) -> Result<i64, String> {
-    // diesel::insert_into(Settings::table)
-    //     .values(&new_setting)
-    //     .get_result(conn)
-
-    // let inserted_id: i32 = diesel::insert_into(crate::schema::Settings::table)
-    //     .values(data)
-    //     .returning(crate::schema::share_data::dsl::id)
-    //     .get_result::<Option<i32>>(db_connection)
-    //     .map_err(|e| format!("Error saving new settings data: {}", e))?
-    //     .unwrap_or(-1);  // Provide a default value if None
-
-    // Ok(inserted_id as i64)
-    Ok(0)
+pub fn create_setting(conn: &mut PooledConnection<ConnectionManager<PgConnection>>, new_setting: SettingsTable) -> QueryResult<SettingsTable> {
+    diesel::insert_into(Settings::table)
+        .values(&new_setting)
+        .get_result(conn)
 }
 
-// pub fn get_setting(conn: &PgConnection, user_key: &str) -> QueryResult<SettingsTable> {
-//     Settings.filter(user_pub_key.eq(user_key))
-//         .first(conn)
-// }
+pub fn get_setting(conn: &mut PooledConnection<ConnectionManager<PgConnection>>, user_key: &str) -> QueryResult<SettingsTable> {
+  
+  
+    Settings.filter(user_pub_key.eq(user_key))
+        .first(conn)
+}
 
-// pub fn update_setting(conn: &PgConnection, user_key: &str, updated_setting: SettingsTable) -> QueryResult<SettingsTable> {
-//     diesel::update(Settings.filter(user_pub_key.eq(user_key)))
-//         .set(&updated_setting)
-//         .get_result(conn)
-// }
+pub fn update_setting(conn: &mut PooledConnection<ConnectionManager<PgConnection>>, user_key: &str, updated_setting: SettingsTable) -> QueryResult<SettingsTable> {
+    
+    diesel::update(Settings.filter(user_pub_key.eq(user_key)))
+        .set((
+            cli_pub_key.eq(updated_setting.cli_pub_key),
+            cli_priv_key.eq(updated_setting.cli_priv_key),
+            witness_network.eq(updated_setting.witness_network),
+            witness_contract_address.eq(updated_setting.witness_contract_address),
+            theme.eq(updated_setting.theme),
+        ))
+        .get_result(conn)
+}
 
 // /// Deletes a record from the `Settings` table based on the `user_pub_key`.
 // ///
@@ -38,7 +39,7 @@ pub fn create_setting(conn: &PgConnection, new_setting: SettingsTable) -> Result
 // ///
 // /// # Returns
 // /// Returns the number of rows deleted (`usize`) or a `diesel::result::Error`.
-// pub fn delete_setting(conn: &PgConnection, user_key: &str) -> QueryResult<usize> {
-//     diesel::delete(Settings.filter(user_pub_key.eq(user_key)))
-//         .execute(conn)
-// }
+pub fn delete_setting(conn: &mut PooledConnection<ConnectionManager<PgConnection>>, user_key: &str) -> QueryResult<usize> {
+    diesel::delete(Settings.filter(user_pub_key.eq(user_key)))
+        .execute(conn)
+}
